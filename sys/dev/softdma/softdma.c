1b9b05607d9af06ca58e844a6751d7b3b7b9af58
Ruslan Bukin <br@bsdpad.com>
2016-12-21 16:36:33 +0000

    add softdma template

---
+++ sys/dev/softdma/softdma.c
@@ -0,0 +1,118 @@
+/*-
+ * Copyright (c) 2016 Ruslan Bukin <br@bsdpad.com>
+ * All rights reserved.
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+ * ("CTSRD"), as part of the DARPA CRASH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include "opt_platform.h"
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/conf.h>
+#include <sys/bus.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/lock.h>
+#include <sys/mutex.h>
+#include <sys/resource.h>
+#include <sys/rman.h>
+
+#include <machine/bus.h>
+
+#ifdef FDT
+#include <dev/fdt/fdt_common.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+#endif
+
+#include <dev/xdma/xdma.h>
+#include "xdma_if.h"
+
+struct softdma_softc {
+	device_t		dev;
+};
+
+static int softdma_probe(device_t dev);
+static int softdma_attach(device_t dev);
+static int softdma_detach(device_t dev);
+
+static int
+softdma_probe(device_t dev)
+{
+
+	if (!ofw_bus_status_okay(dev))
+		return (ENXIO);
+
+	if (!ofw_bus_is_compatible(dev, "freebsd,softdma"))
+		return (ENXIO);
+
+	device_set_desc(dev, "SoftDMA");
+
+	return (BUS_PROBE_DEFAULT);
+}
+
+static int
+softdma_attach(device_t dev)
+{
+	struct softdma_softc *sc;
+
+	sc = device_get_softc(dev);
+	sc->dev = dev;
+
+	return (0);
+}
+
+static int
+softdma_detach(device_t dev)
+{
+	struct softdma_softc *sc;
+
+	sc = device_get_softc(dev);
+
+	return (0);
+}
+
+static device_method_t softdma_methods[] = {
+	/* Device interface */
+	DEVMETHOD(device_probe,			softdma_probe),
+	DEVMETHOD(device_attach,		softdma_attach),
+	DEVMETHOD(device_detach,		softdma_detach),
+
+	DEVMETHOD_END
+};
+
+static driver_t softdma_driver = {
+	"softdma",
+	softdma_methods,
+	sizeof(struct softdma_softc),
+};
+
+static devclass_t softdma_devclass;
+
+DRIVER_MODULE(softdma, simplebus, softdma_driver, softdma_devclass, 0, 0);
