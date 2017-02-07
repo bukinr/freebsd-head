8066165ca58a644ccfe665ca6ea0b91bf86b2a2b
Ruslan Bukin <br@bsdpad.com>
2017-02-07 17:38:05 +0000

    fix build script for office pc

--- build_ci20.sh
+++ build_ci20.sh
@@ -5,7 +5,17 @@ if [ "$1" = "fast" ]; then
     A=FAST
 fi
 
-make -j8 TARGET=mips TARGET_ARCH=mipsel KERN${A}=CI20 buildkernel &&
-scp /home/rb743/obj/mips.mipsel/home/rb743/dev/freebsd-head/sys/CI20/kernel br@128.232.18.121:~/bsdpad/trunk/build/ci20/ || exit 1
+make TARGET=mips TARGET_ARCH=mipsel KERN${A}=CI20 buildkernel || exit 1
 
-ssh br@128.232.18.121 sh /home/br/bsdpad/trunk/build/ci20/ci20.sh
+HOSTNAME=`hostname -s`
+
+if [ "$HOSTNAME" == "vica" ]; then
+	scp /home/rb743/obj/mips.mipsel/home/rb743/dev/freebsd-head/sys/CI20/kernel \
+	    br@128.232.18.121:~/bsdpad/trunk/build/ci20/ || exit 1
+	ssh br@128.232.18.121 sh /home/br/bsdpad/trunk/build/ci20/ci20.sh
+fi
+
+if [ "$HOSTNAME" == "pie" ]; then
+	cp ~/obj/mips.mipsel/usr/home/br/dev/freebsd-head/sys/CI20/kernel /home/br/bsdpad/trunk/build/ci20/
+	sh /home/br/bsdpad/trunk/build/ci20/ci20.sh
+fi
