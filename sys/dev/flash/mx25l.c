8354ea49432f44a5805634e5e1aaad0b3553bb40
Ruslan Bukin <br@bsdpad.com>
2017-04-03 13:20:42 +0100

    cleanup

--- sys/dev/flash/mx25l.c
+++ sys/dev/flash/mx25l.c
@@ -111,11 +111,7 @@ struct mx25l_flash_ident flash_devices[] = {
 	{ "en25p32",	0x1c, 0x2016, 64 * 1024, 64, FL_NONE },
 	{ "en25p64",	0x1c, 0x2017, 64 * 1024, 128, FL_NONE },
 	{ "en25q64",	0x1c, 0x3017, 64 * 1024, 128, FL_ERASE_4K },
-
-	/* Micron */
 	{ "m25p64",	0x20, 0x2017, 64 * 1024, 128, FL_NONE },
-	{ "n25q00aa",	0x20, 0xbb21, 64 * 1024, 2048, FL_NONE },
-
 	{ "mx25ll32",	0xc2, 0x2016, 64 * 1024, 64, FL_NONE },
 	{ "mx25ll64",	0xc2, 0x2017, 64 * 1024, 128, FL_NONE },
 	{ "mx25ll128",	0xc2, 0x2018, 64 * 1024, 256, FL_ERASE_4K | FL_ERASE_32K },
