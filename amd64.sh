d2bc96277b9b9ecbc095607117d180ff6f7bc7f8
Ruslan Bukin <br@bsdpad.com>
2017-04-24 17:59:44 +0100

    add kernel config

--- amd64.sh
+++ amd64.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+A=CONF
+if [ "$1" = "fast" ]; then
+	A=FAST
+fi
+
+make -j8 KERN${A}=SGX buildkernel || exit 1
+
 rm -f disk_amd64_.img disk_amd64_.img.gz
 tools/tools/makeroot/makeroot.sh -s 60m -f amd64_disk/basic.files -e amd64_disk/extras.mtree amd64.img /home/br/world-amd64/ && \
 sudo dd if=amd64.img of=/dev/md0p2 bs=1m || exit 1
