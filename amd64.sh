7a139a9db0717416f08284c1367feeeb996d3fdb
Ruslan Bukin <br@bsdpad.com>
2017-04-24 18:10:29 +0100

    dont build modules

--- amd64.sh
+++ amd64.sh
@@ -5,7 +5,12 @@ if [ "$1" = "fast" ]; then
 	A=FAST
 fi
 
+export DESTDIR=/home/br/world-amd64
+
 make -j8 KERN${A}=SGX buildkernel || exit 1
+
+#make -j8 -DNO_ROOT KERN${A}=SGX installkernel || exit 1
+cp /home/br/obj/usr/home/br/dev/freebsd-head/sys/SGX/kernel $DESTDIR/boot/kernel/kernel
 
 rm -f disk_amd64_.img disk_amd64_.img.gz
 tools/tools/makeroot/makeroot.sh -s 60m -f amd64_disk/basic.files -e amd64_disk/extras.mtree amd64.img /home/br/world-amd64/ && \
