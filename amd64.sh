24041ee4f3a2b6144bc34cde92d5c05ad94eb35e
Ruslan Bukin <br@bsdpad.com>
2017-04-24 17:47:12 +0100

    add scripts

---
+++ amd64.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -f disk_amd64_.img disk_amd64_.img.gz
+tools/tools/makeroot/makeroot.sh -s 60m -f amd64_disk/basic.files -e amd64_disk/extras.mtree amd64.img /home/br/world-amd64/ && \
+sudo dd if=amd64.img of=/dev/md0p2 bs=1m || exit 1
+cp disk_amd64.img disk_amd64_.img
+gzip disk_amd64_.img
+scp disk_amd64_.img.gz 10.5.0.85:~/
