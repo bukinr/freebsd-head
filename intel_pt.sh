0e08768d82d80ab7f197fabd52698e8ad94af0a2
Ruslan Bukin <br@bsdpad.com>
2017-08-30 18:26:43 +0100

    build mtrace

--- intel_pt.sh
+++ intel_pt.sh
@@ -5,6 +5,9 @@ if [ "$1" = "fast" ]; then
     A=FAST
 fi
 
+VARS=`make buildenvvars`
+eval $VARS make -C usr.sbin/mtrace || exit 1
+
 make -j8 KERN${A}=PT buildkernel || exit 1
 
-scp /home/br/obj/usr/home/br/dev/freebsd-head/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/pt/pt.ko 10.5.0.86:~/
+scp usr.sbin/mtrace/mtrace /home/br/obj/usr/home/br/dev/freebsd-head/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/pt/pt.ko 10.5.0.86:~/
