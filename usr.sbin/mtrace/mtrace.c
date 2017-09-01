a8d0d0c0871d735b33bf46cf56a3781a552f8168
Ruslan Bukin <br@bsdpad.com>
2017-09-01 18:42:50 +0100

    print some packets

--- usr.sbin/mtrace/mtrace.c
+++ usr.sbin/mtrace/mtrace.c
@@ -47,11 +47,17 @@ __FBSDID("$FreeBSD$");
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 #include <unistd.h>
 #include <wchar.h>
 #include <wctype.h>
 
 #include <gelf.h>
+
+#include <libipt/pt_cpu.h>
+#include <libipt/pt_last_ip.h>
+#include <libipt/pt_time.h>
+#include <libipt/pt_compiler.h>
 #include <libipt/intel-pt.h>
 
 #define	round_up(x,y) (((x) + (y) - 1) & ~((y)-1))
@@ -65,6 +71,215 @@ struct pt_test {
 
 #define	PT_IOC_TEST \
 	_IOW(PT_MAGIC, 0x00, struct pt_test)
+
+#define print_field(field, ...)					\
+	do {							\
+		/* Avoid partial overwrites. */			\
+		memset(field, 0, sizeof(field));		\
+		snprintf(field, sizeof(field), __VA_ARGS__);	\
+	} while (0)
+
+static uint64_t
+sext(uint64_t val, uint8_t sign)
+{
+	uint64_t signbit, mask;
+
+	signbit = 1ull << (sign - 1);
+	mask = ~0ull << sign;
+
+	return val & signbit ? val | mask : val & ~mask;
+}
+
+
+struct ptdump_buffer {
+	char offset[17];
+	char opcode[10];
+	union {
+		char standard[25];
+		char extended[48];
+	} payload;
+};
+
+static int
+print_tnt_payload(struct ptdump_buffer *buffer, uint64_t offset __unused,
+    const struct pt_packet_tnt *packet)
+{
+
+	uint64_t tnt;
+	uint8_t bits;
+	char *begin, *end;
+
+	bits = packet->bit_size;
+	tnt = packet->payload;
+	begin = buffer->payload.extended;
+	end = begin + bits;
+
+	if (sizeof(buffer->payload.extended) < bits) {
+		/* Truncating tnt payload */
+		end = begin + sizeof(buffer->payload.extended);
+	}
+
+	for (; begin < end; ++begin, --bits)
+		*begin = tnt & (1ull << (bits - 1)) ? '!' : '.';
+
+	return (0);
+}
+
+static int
+print_ip_payload(struct ptdump_buffer *buffer, uint64_t offset __unused,
+    const struct pt_packet_ip *packet)
+{
+
+	switch (packet->ipc) {
+	case pt_ipc_suppressed:
+		print_field(buffer->payload.standard, "%x: ????????????????",
+			    pt_ipc_suppressed);
+		return 0;
+
+	case pt_ipc_update_16:
+		//printf("%s: %lx\n", __func__, packet->ip);
+		print_field(buffer->payload.standard, "%x: ????????????%04"
+			    PRIx64, pt_ipc_update_16, packet->ip);
+		return 0;
+
+	case pt_ipc_update_32:
+		print_field(buffer->payload.standard, "%x: ????????%08"
+			    PRIx64, pt_ipc_update_32, packet->ip);
+		return 0;
+
+	case pt_ipc_update_48:
+		print_field(buffer->payload.standard, "%x: ????%012"
+			    PRIx64, pt_ipc_update_48, packet->ip);
+		return 0;
+
+	case pt_ipc_sext_48:
+		print_field(buffer->payload.standard, "%x: %016" PRIx64,
+			    pt_ipc_sext_48, sext(packet->ip, 48));
+		return 0;
+
+	case pt_ipc_full:
+		print_field(buffer->payload.standard, "%x: %016" PRIx64,
+			    pt_ipc_full, packet->ip);
+		return 0;
+	default:
+		printf("unknown ipc\n");
+	}
+
+	print_field(buffer->payload.standard, "%x: %016" PRIx64,
+	    packet->ipc, packet->ip);
+}
+
+static int
+init_ipt(void *base)
+{
+	struct pt_packet_decoder *decoder;
+	struct pt_config config;
+	int error;
+
+	memset(&config, 0, sizeof(config));
+	pt_config_init(&config);
+
+	error = pt_cpu_read(&config.cpu);
+	printf("err %d\n", error);
+	error = pt_cpu_errata(&config.errata, &config.cpu);
+	printf("err %d\n", error);
+
+	//config->begin = buffer;
+	//config->end = buffer + size;
+	config.begin = (uint8_t *)base;
+	config.end = (uint8_t *)((uint64_t)base + (2*1024*1024));
+
+	decoder = pt_pkt_alloc_decoder(&config);
+	if (decoder == NULL)
+		printf("Can't allocate decoder\n");
+	else
+		printf("Decoder allocated\n");
+
+	//error = pt_pkt_sync_set(decoder, 0ull);
+	error = pt_pkt_sync_forward(decoder);
+
+	uint64_t offset;
+	struct pt_packet packet;
+	struct ptdump_buffer buffer;
+	const char *sep;
+
+	sep = "";
+
+	while (1) {
+		error = pt_pkt_get_offset(decoder, &offset);
+		if (error < 0)
+			break;
+		//printf("err %d, offset 0x%lx\n", error, offset);
+
+		error = pt_pkt_next(decoder, &packet, sizeof(packet));
+		if (error < 0)
+			break;
+		//printf("err %d, packet.type %d\n", error, packet.type);
+
+
+		memset(&buffer, 0, sizeof(buffer));
+		//print_field(buffer.offset, "%016" PRIx64, offset);
+
+		switch (packet.type) {
+		case ppt_psb:
+			print_field(buffer.opcode, "psb");
+			break;
+		case ppt_psbend:
+			print_field(buffer.opcode, "psbend");
+			break;
+		case ppt_pad:
+			continue;
+			print_field(buffer.opcode, "pad");
+			break;
+		case ppt_fup:
+			continue;
+			print_field(buffer.opcode, "fup");
+			break;
+		case ppt_tip:
+			print_field(buffer.opcode, "tip");
+			print_ip_payload(&buffer, offset, &packet.payload.ip);
+			break;
+		case ppt_tip_pge:
+			print_field(buffer.opcode, "tip_pge");
+			print_ip_payload(&buffer, offset, &packet.payload.ip);
+			break;
+		case ppt_tip_pgd:
+			print_field(buffer.opcode, "tip_pgd");
+			print_ip_payload(&buffer, offset, &packet.payload.ip);
+			break;
+		case ppt_mode:
+			print_field(buffer.opcode, "mode");
+			break;
+		case ppt_tsc:
+			print_field(buffer.opcode, "tsc");
+			print_field(buffer.payload.standard, "%" PRIx64,
+			    packet.payload.tsc.tsc);
+			break;
+		case ppt_cbr:
+			print_field(buffer.opcode, "cbr");
+			break;
+		case ppt_tnt_8:
+			continue;
+			print_field(buffer.opcode, "tnt.8");
+			print_tnt_payload(&buffer, offset, &packet.payload.tnt);
+			break;
+		case ppt_tnt_64:
+			continue;
+			print_field(buffer.opcode, "tnt.64");
+			print_tnt_payload(&buffer, offset, &packet.payload.tnt);
+			break;
+		default:
+			printf("unknown packet %d\n", packet.type);
+			break;
+		}
+
+		printf("%s%s: ", sep, buffer.opcode);
+		//printf(" %s\n", buffer.payload.extended);
+		printf(" %s\n", buffer.payload.standard);
+	}
+
+	return (0);
+}
 
 int
 main(int argc __unused, char *argv[] __unused)
@@ -115,17 +330,13 @@ main(int argc __unused, char *argv[] __unused)
 		/* Parent */
 		//error = ioctl(fd, PT_IOC_TEST, &data);
 		sleep(2);
+
+		init_ipt(base);
+
 		printf("while loop\n");
-		while (1)
-			printf("%lx %lx %lx %lx %lx\n", addr[0], addr[1], addr[2], addr[3], addr[4]);
+		while (1);
+		//printf("%lx %lx %lx %lx %lx\n", addr[0], addr[1], addr[2], addr[3], addr[4]);
 	}
-
-	struct pt_packet_decoder *decoder;
-	struct pt_config config;
-
-	memset(&config, 0, sizeof(config));
-	pt_config_init(&config);
-	decoder = pt_pkt_alloc_decoder(&config);
 
 	return (0);
 }
