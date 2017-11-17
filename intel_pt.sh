d354333eac4084f80032b7962ba200e2b27ed286
Ruslan Bukin <br@bsdpad.com>
2017-11-17 14:21:55 +0000

    sync with GENERIC

--- intel_pt.sh
+++ intel_pt.sh
@@ -26,12 +26,11 @@ eval $VARS make -C usr.sbin/pmctrace clean all || exit 1
 # /home/br/obj/usr/home/br/dev/freebsd-head/usr.sbin/mtrace/mtrace
 
 scp \
- /home/br/obj/usr/home/br/dev/freebsd-head/usr.sbin/pmcstat/pmcstat \
- /home/br/obj/usr/home/br/dev/freebsd-head/usr.sbin/pmctrace/pmctrace \
- /home/br/obj/usr/home/br/dev/freebsd-head/lib/libpmc/libpmc.so.5 \
- /home/br/obj/usr/home/br/dev/freebsd-head/lib/libipt/libipt.so.0 \
- /home/br/obj/usr/home/br/dev/freebsd-head/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/hwpmc/hwpmc.ko \
- /home/br/obj/usr/home/br/dev/freebsd-head/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/pt/pt.ko \
- /home/br/obj/usr/home/br/dev/freebsd-head/sys/PT/kernel \
+ /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/usr.sbin/pmcstat/pmcstat \
+ /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/usr.sbin/pmctrace/pmctrace \
+ /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/lib/libpmc/libpmc.so.5 \
+ /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/lib/libipt/libipt.so.0 \
+ /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/hwpmc/hwpmc.ko \
+ /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/sys/PT/kernel \
  10.5.0.86:~/
 
