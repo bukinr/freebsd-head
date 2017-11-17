2ad9dccd8305edc6918a2ad1a8f69369f5cb0be4
Ruslan Bukin <br@bsdpad.com>
2017-11-17 15:41:47 +0000

    don't forget specialreg.h changes

--- patch.pt.sh
+++ patch.pt.sh
@@ -1,1 +1,1 @@
-git diff -U9999999 origin/master lib/libpmc sys/amd64/include sys/conf sys/dev/hwpmc sys/kern sys/modules sys/sys usr.sbin > patch.pt
+git diff -U9999999 origin/master lib/libpmc sys/amd64/include sys/x86 sys/conf sys/dev/hwpmc sys/kern sys/modules sys/sys usr.sbin > patch.pt
