c067570fc5a6fd0496002c427aa3e98ed27bd0bf
Ruslan Bukin <br@bsdpad.com>
2017-12-20 16:19:55 +0000

    improve script

--- arm64.sh
+++ arm64.sh
@@ -6,7 +6,10 @@ fi
 make -j6 TARGET=arm64 KERN${A}=GENERIC buildkernel || exit 1
 cp /home/br/obj/usr/home/br/dev/freebsd-head/arm64.aarch64/sys/GENERIC/kernel /tftpboot/
 
-echo setenv serverip 10.5.0.1 ; setenv ipaddr 10.5.0.44; usb start; tftpboot kernel; fatwrite mmc 1 0x81000000 kernel $filesize; tftpboot 0x81000000 loader.efi; tftpboot 0x83000000 dragonboard410c.dtb; usb stop; bootefi 0x81000000 0x83000000
+echo "setenv serverip 10.5.0.1 ; setenv ipaddr 10.5.0.44; usb start; tftpboot kernel; fatwrite mmc 1 0x81000000 kernel \$filesize; tftpboot 0x81000000 loader.efi; tftpboot 0x83000000 dragonboard410c.dtb; usb stop; bootefi 0x81000000 0x83000000"
 
-echo load /kernel
-echo boot
+echo "load /kernel"
+echo "boot"
+
+# dtc -o - -O dts -I dtb /tftpboot/dragonboard410c.dtb | less
+
