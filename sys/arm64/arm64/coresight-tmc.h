d2d56f39eda73b83915d08d20318109bb5897416
Ruslan Bukin <br@bsdpad.com>
2018-01-12 15:31:48 +0000

    add tmc registers

---
+++ sys/arm64/arm64/coresight-tmc.h
@@ -0,0 +1,50 @@
+#define	TMC_RSZ		0x004 /* RAM Size Register */
+#define	TMC_STS		0x00C /* Status Register */
+#define	TMC_RRD		0x010 /* RAM Read Data Register */
+#define	TMC_RRP		0x014 /* RAM Read Pointer Register */
+#define	TMC_RWP		0x018 /* RAM Write Pointer Register */
+#define	TMC_TRG		0x01C /* Trigger Counter Register */
+#define	TMC_CTL		0x020 /* Control Register */
+#define	TMC_RWD		0x024 /* RAM Write Data Register */
+#define	TMC_MODE	0x028 /* Mode Register */
+#define	TMC_LBUFLEVEL	0x02C /* Latched Buffer Fill Level */
+#define	TMC_CBUFLEVEL	0x030 /* Current Buffer Fill Level */
+#define	TMC_BUFWM	0x034 /* Buffer Level Water Mark */
+#define	TMC_RRPHI	0x038 /* RAM Read Pointer High Register */
+#define	TMC_RWPHI	0x03C /* RAM Write Pointer High Register */
+#define	TMC_AXICTL	0x110 /* AXI Control Register */
+#define	TMC_DBALO	0x118 /* Data Buffer Address Low Register */
+#define	TMC_DBAHI	0x11C /* Data Buffer Address High Register */
+#define	TMC_FFSR	0x300 /* Formatter and Flush Status Register */
+#define	TMC_FFCR	0x304 /* Formatter and Flush Control Register */
+#define	TMC_PSCR	0x308 /* Periodic Synchronization Counter Register */
+#define	TMC_ITATBMDATA0	0xED0 /* Integration Test ATB Master Data Register 0 */
+#define	TMC_ITATBMCTR2	0xED4 /* Integration Test ATB Master Interface Control 2 Register */
+#define	TMC_ITATBMCTR1	0xED8 /* Integration Test ATB Master Control Register 1 */
+#define	TMC_ITATBMCTR0	0xEDC /* Integration Test ATB Master Interface Control 0 Register */
+#define	TMC_ITMISCOP0	0xEE0 /* Integration Test Miscellaneous Output Register 0 */
+#define	TMC_ITTRFLIN	0xEE8 /* Integration Test Trigger In and Flush In Register */
+#define	TMC_ITATBDATA0	0xEEC /* Integration Test ATB Data Register 0 */
+#define	TMC_ITATBCTR2	0xEF0 /* Integration Test ATB Control 2 Register */
+#define	TMC_ITATBCTR1	0xEF4 /* Integration Test ATB Control 1 Register */
+#define	TMC_ITATBCTR0	0xEF8 /* Integration Test ATB Control 0 Register */
+#define	TMC_ITCTRL	0xF00 /* Integration Mode Control Register */
+#define	TMC_CLAIMSET	0xFA0 /* Claim Tag Set Register */
+#define	TMC_CLAIMCLR	0xFA4 /* Claim Tag Clear Register */
+#define	TMC_LAR		0xFB0 /* Lock Access Register */
+#define	TMC_LSR		0xFB4 /* Lock Status Register */
+#define	TMC_AUTHSTATUS	0xFB8 /* Authentication Status Register */
+#define	TMC_DEVID	0xFC8 /* Device Configuration Register */
+#define	TMC_DEVTYPE	0xFCC /* Device Type Identifier Register */
+#define	TMC_PERIPHID4	0xFD0 /* Peripheral ID4 Register */
+#define	TMC_PERIPHID5	0xFD4 /* Peripheral ID5 Register */
+#define	TMC_PERIPHID6	0xFD8 /* Peripheral ID6 Register */
+#define	TMC_PERIPHID7	0xFDC /* Peripheral ID7 Register */
+#define	TMC_PERIPHID0	0xFE0 /* Peripheral ID0 Register */
+#define	TMC_PERIPHID1	0xFE4 /* Peripheral ID1 Register */
+#define	TMC_PERIPHID2	0xFE8 /* Peripheral ID2 Register */
+#define	TMC_PERIPHID3	0xFEC /* Peripheral ID3 Register */
+#define	TMC_COMPID0	0xFF0 /* Component ID0 Register */
+#define	TMC_COMPID1	0xFF4 /* Component ID1 Register */
+#define	TMC_COMPID2	0xFF8 /* Component ID2 Register */
+#define	TMC_COMPID3	0xFFC /* Component ID3 Register */
