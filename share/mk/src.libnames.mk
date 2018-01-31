e016fdceecd86251a6b376a5d715abdff04b199d
Ruslan Bukin <br@bsdpad.com>
2018-01-31 13:13:01 +0000

    add c++

--- share/mk/src.libnames.mk
+++ share/mk/src.libnames.mk
@@ -70,6 +70,7 @@ _LIBRARIES=	\
 		bz2 \
 		c \
 		c_pic \
+		c++ \
 		calendar \
 		cam \
 		casper \
@@ -311,6 +312,7 @@ _DP_gssapi_krb5+=	gssapi krb5 crypto roken asn1 com_err
 _DP_lzma=	pthread
 _DP_ucl=	m
 _DP_vmmapi=	util
+_DP_opencsd=	cxxrt c++
 _DP_ctf=	z
 _DP_dtrace=	ctf elf proc pthread rtld_db
 _DP_xo=		util
