528074aabdd585da7239f947a245d4710fdc1d8c
Ruslan Bukin <br@bsdpad.com>
2018-03-07 16:35:35 +0000

    add copyright block

--- sys/arm64/coresight/coresight_if.m
+++ sys/arm64/coresight/coresight_if.m
@@ -2,6 +2,10 @@
 # Copyright (c) 2018 Ruslan Bukin <br@bsdpad.com>
 # All rights reserved.
 #
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
