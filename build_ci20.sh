12cb24efa9d44214709ddeca5f940a68979e9b27
Ruslan Bukin <br@bsdpad.com>
2018-04-03 15:49:25 +0100

    fix script

--- build_ci20.sh
+++ build_ci20.sh
@@ -5,6 +5,22 @@ if [ "$1" = "fast" ]; then
     A=FAST
 fi
 
+export MAKEOBJDIRPREFIX=/home/br/obj/
+
+export CROSS_BINUTILS_PREFIX=/usr/local/bin/mips-unknown-freebsd11.1-
+export XCC=${CROSS_BINUTILS_PREFIX}gcc
+export XCXX=${CROSS_BINUTILS_PREFIX}c++
+export XCPP=${CROSS_BINUTILS_PREFIX}cpp
+
+export CROSS_BINUTILS_PREFIX=/usr/local/bin/mips-unknown-freebsd11.1-
+export STRIPBIN=${CROSS_BINUTILS_PREFIX}strip
+
+export WITHOUT_CLANG=yes
+export WITHOUT_CLANG_BOOTSTRAP=yes
+export WITHOUT_CLANG_FULL=yes
+export WITHOUT_CLANG_IS_CC=yes
+export X_COMPILER_TYPE=gcc
+
 make TARGET=mips TARGET_ARCH=mipsel KERN${A}=CI20 buildkernel || exit 1
 
 HOSTNAME=`hostname -s`
@@ -16,6 +32,6 @@ if [ "$HOSTNAME" == "vica" ]; then
 fi
 
 if [ "$HOSTNAME" == "pie" ]; then
-	cp ~/obj/mips.mipsel/usr/home/br/dev/freebsd-head/sys/CI20/kernel /home/br/bsdpad/trunk/build/ci20/
+	cp /home/br/obj/usr/home/br/dev/freebsd-head/mips.mipsel/sys/CI20/kernel /home/br/bsdpad/trunk/build/ci20/
 	sh /home/br/bsdpad/trunk/build/ci20/ci20.sh
 fi
