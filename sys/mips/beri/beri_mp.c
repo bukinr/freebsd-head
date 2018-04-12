41ba1d36e173efaef205f69badc418d89a4cf1a1
Ruslan Bukin <br@bsdpad.com>
2018-04-12 17:38:30 +0100

    revert to minimize changes

--- sys/mips/beri/beri_mp.c
+++ sys/mips/beri/beri_mp.c
@@ -166,8 +166,9 @@ platform_init_secondary(int cpuid)
 	beripic_setup_ipi(ic, cpuid, ipi);
 
 	/* Unmask the interrupt */
-	if (cpuid != 0)
+	if (cpuid != 0) {
 		mips_wr_status(mips_rd_status() | (((1 << ipi) << 8) << 2));
+	}
 }
 
 
