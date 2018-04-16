cfedd00a1627bd869b67960f3b674f96658d0e55
Ruslan Bukin <br@bsdpad.com>
2018-04-16 14:37:13 +0100

    add patch generating script

---
+++ patch.hwpmc.sh
@@ -0,0 +1,30 @@
+$!/bin/sh
+
+# 1 - core
+git diff -U99999999 origin/master sys/kern sys/sys sys/dev/hwpmc/hwpmc_mod.c > patch.hwpmc1
+
+# 2 - vm
+git diff -U99999999 origin/master sys/conf/files sys/dev/hwpmc/hwpmc_vm.c \
+	sys/dev/hwpmc/hwpmc_vm.h sys/modules/hwpmc/Makefile > patch.hwpmc2
+
+# 3 -- libpmc
+git diff -U99999999 origin/master lib/libpmc sys/dev/hwpmc/pmc_events.h > patch.hwpmc3
+
+# 4 -- coresight kernel
+git diff -U99999999 origin/master sys/arm64/include/pmc_mdep.h sys/conf/files.arm64 \
+	sys/dev/hwpmc/hwpmc_arm64.c sys/dev/hwpmc/hwpmc_cs.c sys/dev/hwpmc/hwpmc_cs.h > patch.hwpmc4
+
+# 5 -- intel pt
+git diff -U99999999 origin/master sys/amd64/include/pmc_mdep.h sys/conf/files.amd64 \
+	sys/dev/hwpmc/hwpmc_core.c sys/dev/hwpmc/hwpmc_intel.c sys/dev/hwpmc/hwpmc_pt.c \
+	sys/dev/hwpmc/hwpmc_pt.h > patch.hwpmc5
+
+# 6 - pmctrace
+git diff -U99999999 origin/master usr.sbin/Makefile usr.sbin/pmctrace/Makefile \
+	usr.sbin/pmctrace/pmctrace.c usr.sbin/pmctrace/pmctrace.h > patch.hwpmc6
+
+# 7 - pmctrace PT
+git diff -U99999999 origin/master usr.sbin/pmctrace/pmctrace_pt.* > patch.hwpmc7
+
+# 8 - pmctrace CS
+git diff -U99999999 origin/master usr.sbin/pmctrace/pmctrace_cs.* > patch.hwpmc8
