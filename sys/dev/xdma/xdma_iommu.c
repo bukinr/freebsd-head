2a998da91d10ba4a8089e28ed7dac8f8ae2ccac7
Ruslan Bukin <br@bsdpad.com>
2019-06-28 15:35:27 +0100

    Add xdma iommu test code.

---
+++ sys/dev/xdma/xdma_iommu.c
@@ -0,0 +1,142 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Ruslan Bukin <br@bsdpad.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include "opt_platform.h"
+#include <sys/param.h>
+#include <sys/conf.h>
+#include <sys/bus.h>
+#include <sys/kernel.h>
+#include <sys/lock.h>
+#include <sys/malloc.h>
+#include <sys/mbuf.h>
+#include <sys/mutex.h>
+#include <sys/rwlock.h>
+
+#include <machine/cache.h>
+#include <machine/bus.h>
+
+#include <vm/vm.h>
+#include <vm/pmap.h>
+#include <vm/vm_extern.h>
+#include <vm/vm_page.h>
+
+#include <mips/beri/beri_manager.h>
+#include <mips/beri/beri_iommu.h>
+
+#ifdef FDT
+#include <dev/fdt/fdt_common.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+#endif
+
+#include <dev/xdma/xdma.h>
+
+struct pmap msgdma_pmap;
+vmem_t *vmem;
+
+void
+iommu_remove_entry(xdma_channel_t *xchan, vm_offset_t va)
+{
+	vm_offset_t va1;
+
+	va1 = va & ~(PAGE_SIZE - 1);
+
+	beri_iommu_invalidate(va);
+#if 0
+	int err;
+	printf("%s: %lx\n", __func__, va1);
+
+	err = dm_invalidate_tlb(va, 8192);
+	if (err)
+		printf("%s: failed\n", __func__);
+#endif
+
+	vmem_free(vmem, va1, 8192);
+}
+
+void
+iommu_add_entry(xdma_channel_t *xchan, vm_offset_t *va,
+    vm_size_t size, vm_paddr_t pa)
+{
+	vm_offset_t addr;
+
+	size = roundup2(size, PAGE_SIZE * 2);
+
+	if (vmem_alloc(vmem, size,
+	    M_BESTFIT | M_NOWAIT, &addr)) {
+		panic("cant allocate memory\n");
+	}
+
+	addr |= pa & (PAGE_SIZE - 1);
+
+	*va = addr;
+
+	printf("%s: va %lx size %lx pa %lx\n",
+	    __func__, addr, size, pa);
+	iommu_kenter_device(&msgdma_pmap, addr, size, pa);
+}
+
+int
+iommu_init(void)
+{
+	pd_entry_t *segmap;
+	uint64_t addr;
+	int i;
+
+	addr = 0x11000000;
+
+	printf("%s\n", __func__);
+
+	segmap = (void *)MIPS_PHYS_TO_XKPHYS_UNCACHED(addr);
+	for (i = 0; i < 512; i++)
+		segmap[i] = NULL;
+
+#if 0
+	addr += 0x1000;
+	for (i = 0; i < 512; i++) {
+		segmap[i] = (void *)MIPS_PHYS_TO_XKPHYS_UNCACHED(addr);
+		*segmap[i] = 0;
+		addr += 0x1000;
+	}
+#endif
+
+	printf("allocating vmem\n");
+
+	vmem = vmem_create("xDMA vmem", 0, 0, PAGE_SIZE * 2,
+	    PAGE_SIZE * 2, M_BESTFIT | M_WAITOK);
+	if (vmem == NULL)
+		return (-1);
+
+	vmem_add(vmem, 0xC000000000000000, (1ULL << 39), 0);
+
+	msgdma_pmap.pm_segtab = segmap;
+
+	return (0);
+}
