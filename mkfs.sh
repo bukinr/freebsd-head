b406c3c810359258c84840d539af23d13cc2ee46
Ruslan Bukin <br@bsdpad.com>
2019-07-15 15:11:44 +0100

    increase fs size

--- mkfs.sh
+++ mkfs.sh
@@ -1,1 +1,1 @@
-tools/tools/makeroot/makeroot.sh -B big -s 20m -f basic.files mips.img /home/br/world-mips64
+tools/tools/makeroot/makeroot.sh -B big -s 24m -f basic.files mips.img /home/br/world-mips64
