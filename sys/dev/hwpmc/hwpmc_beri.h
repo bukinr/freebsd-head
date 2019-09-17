ea6caae9d81076ba081285b14bcc40460b22ee48
Ruslan Bukin <br@bsdpad.com>
2019-09-17 14:21:23 +0100

    add copyright

--- sys/dev/hwpmc/hwpmc_beri.h
+++ sys/dev/hwpmc/hwpmc_beri.h
@@ -1,8 +1,48 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2019 Alex Richardson
+ *
+ * This software was developed by SRI International and the University of
+ * Cambridge Computer Laboratory (Department of Computer Science and
+ * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+ * DARPA SSITH research programme.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $FreeBSD$
+ */
+
+#ifndef	_DEV_HWPMC_HWPMC_BERI_H_
+#define	_DEV_HWPMC_HWPMC_BERI_H_
+
 #define STATCOUNTER_ITEM(name, X, Y)				\
 static inline uint64_t statcounters_get_##name##_count(void)	\
 {								\
 	uint64_t ret;						\
-	__asm __volatile(".word (0x1f << 26) | (0x0 << 21) | (12 << 16) | ("#X" << 11) | ( "#Y"  << 6) | 0x3b\n\t"				\
+	__asm __volatile(					\
+			".word (0x1f << 26) | (0x0 << 21) |	\
+				(12 << 16) | ("#X" << 11) |	\
+				( "#Y"  << 6) | 0x3b\n\t"	\
 			"move %0,$12" : "=r" (ret) :: "$12");	\
 	return (ret);						\
 }
@@ -63,3 +103,5 @@ STATCOUNTER_ITEM(tagcachemaster_write_req_flit,14,2)
 STATCOUNTER_ITEM(tagcachemaster_read_rsp,14,3)
 STATCOUNTER_ITEM(tagcachemaster_read_rsp_flit,14,4)
 STATCOUNTER_ITEM(tagcachemaster_write_rsp,14,5)
+
+#endif	/* !_DEV_HWPMC_HWPMC_BERI_H_ */
