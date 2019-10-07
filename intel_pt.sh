4edc940c59c002da24e06b1d8b3a96bcf5c4ad04
Ruslan Bukin <br@bsdpad.com>
2019-10-07 16:45:42 +0100

    fix paths

--- intel_pt.sh
+++ intel_pt.sh
@@ -31,11 +31,10 @@ eval $VARS make -C usr.sbin/pmctrace clean all || exit 1
 ssh 10.5.0.86 sudo killall pmcstat
 
 scp \
- /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/usr.sbin/pmcstat/pmcstat \
- /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/usr.sbin/pmctrace/pmctrace \
- /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/lib/libpmc/libpmc.so.5 \
- /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/lib/libipt/libipt.so.0 \
- /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/hwpmc/hwpmc.ko \
- /home/br/obj/usr/home/br/dev/freebsd-head/amd64.amd64/sys/PT/kernel \
+ /xhome/obj/usr/home/br/dev/freebsd-head/amd64.amd64/usr.sbin/pmcstat/pmcstat \
+ /xhome/obj/usr/home/br/dev/freebsd-head/amd64.amd64/usr.sbin/pmctrace/pmctrace\
+ /xhome/obj/usr/home/br/dev/freebsd-head/amd64.amd64/lib/libpmc/libpmc.so.5 \
+ /xhome/obj/usr/home/br/dev/freebsd-head/amd64.amd64/lib/libipt/libipt.so.0 \
+ /xhome/obj/usr/home/br/dev/freebsd-head/amd64.amd64/sys/PT/modules/usr/home/br/dev/freebsd-head/sys/modules/hwpmc/hwpmc.ko \
+ /xhome/obj/usr/home/br/dev/freebsd-head/amd64.amd64/sys/PT/kernel \
  10.5.0.86:~/
-
